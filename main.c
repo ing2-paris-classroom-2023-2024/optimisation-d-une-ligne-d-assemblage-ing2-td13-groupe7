a52e2ef1fa8f7a7a81a50776e48a5905acbfd2e9
gauthier <gauthier.million@edu.ece.fr>
2023-11-26 22:19:52 +0100

    optimisation des contraintes, deuxieme jet

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_OPERATIONS 100 // Nombre maximal d'opérations
+#define MAX_STATIONS 10    // Nombre maximal de stations
 
 // Structure pour représenter un nœud dans le graphe
 struct Node {
     int id;
+    int duration; // Durée de l'opération
     // Autres informations pertinentes
 };
 
@@ -13,23 +18,50 @@ struct Edge {
     // Autres informations pertinentes
 };
 
+struct Node operations[MAX_OPERATIONS]; // Tableau des opérations
+struct Edge edges[MAX_OPERATIONS * MAX_OPERATIONS]; // Tableau des arcs entre les opérations
+int edgeCount = 0; // Compteur du nombre d'arcs
+
+// Fonction pour ajouter un arc au graphe
+void addEdge(int source, int destination) {
+    edges[edgeCount].source_id = source;
+    edges[edgeCount].destination_id = destination;
+    edgeCount++;
+}
+
 // Fonction pour lire les données des fichiers texte et construire le graphe
 void construireGraphe() {
-    // Lire les données des fichiers texte et créer les nœuds et les arcs du graphe
+    // Ici, vous devriez lire les données des fichiers texte
+    // et remplir le tableau d'opérations ainsi que les contraintes
+
+    // Exemple simplifié : opérations et contraintes statiques
+    int nbOperations = 6;
+    operations[0].id = 0; operations[0].duration = 3;
+    operations[1].id = 1; operations[1].duration = 4;
+    operations[2].id = 2; operations[2].duration = 2;
+    operations[3].id = 3; operations[3].duration = 5;
+    operations[4].id = 4; operations[4].duration = 2;
+    operations[5].id = 5; operations[5].duration = 3;
+
+    addEdge(0, 1); // Exemple de contrainte de précédence : opération 0 doit précéder opération 1
+    addEdge(0, 2);
+    addEdge(1, 3);
+    addEdge(2, 3);
+    addEdge(3, 4);
+    addEdge(3, 5);
 }
 
 // Algorithme de tri topologique pour l'ordonnancement des opérations
 void triTopologique() {
-    // Implémenter l'algorithme de tri topologique sur le graphe
+    // Implémentation de l'algorithme de tri topologique (par exemple, DFS ou BFS)
 }
 
 // Allocation des opérations aux stations en respectant les contraintes
 void repartitionStations() {
-    // Implémenter l'allocation des opérations aux stations
+    // Implémentation de l'allocation des opérations aux stations
 }
 
 int main() {
-    // Étapes principales du programme
     construireGraphe();
     triTopologique();
     repartitionStations();
