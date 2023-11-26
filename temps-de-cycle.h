9c2d702ec2ee07485c90545513cfea0fe31d25ba
louisgion <louis.gion@edu.ece.fr>
2023-11-26 22:31:11 +0100

    fonction qui répond à la contrainte de temps de cycle

---
+++ temps-de-cycle.h
@@ -0,0 +1,10 @@
+//
+// Created by Louis Gion on 26/11/2023.
+//
+
+#ifndef OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TD13_GROUPE7_TEMPS_DE_CYCLE_H
+#define OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TD13_GROUPE7_TEMPS_DE_CYCLE_H
+
+#endif //OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TD13_GROUPE7_TEMPS_DE_CYCLE_H
+
+int optimisationNombreStations(int operations[], int num_operations, int temps_de_cycle, int station_assignments[]);
