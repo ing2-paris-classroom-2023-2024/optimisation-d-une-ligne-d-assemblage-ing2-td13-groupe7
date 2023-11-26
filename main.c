fd0205e2bb9ead77b417611e4a9e4105807ac2b0
gauthier <gauthier.million@edu.ece.fr>
2023-11-26 22:16:36 +0100

    optimisation des contraintes, premier jet

--- main.c
+++ main.c
@@ -1,11 +1,38 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+// Structure pour représenter un nœud dans le graphe
+struct Node {
+    int id;
+    // Autres informations pertinentes
+};
+
+// Structure pour représenter un arc entre deux nœuds
+struct Edge {
+    int source_id;
+    int destination_id;
+    // Autres informations pertinentes
+};
+
+// Fonction pour lire les données des fichiers texte et construire le graphe
+void construireGraphe() {
+    // Lire les données des fichiers texte et créer les nœuds et les arcs du graphe
 }
 
+// Algorithme de tri topologique pour l'ordonnancement des opérations
+void triTopologique() {
+    // Implémenter l'algorithme de tri topologique sur le graphe
+}
 
+// Allocation des opérations aux stations en respectant les contraintes
+void repartitionStations() {
+    // Implémenter l'allocation des opérations aux stations
+}
 
-//test github//
+int main() {
+    // Étapes principales du programme
+    construireGraphe();
+    triTopologique();
+    repartitionStations();
 
+    return 0;
+}
